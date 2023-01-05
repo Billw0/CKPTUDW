b2b747af3d9e82ee4a7390d740a5113042368551
thuan-NQT <92578416+SwowS-bot@users.noreply.github.com>
2023-01-05 11:13:33 +0700

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,151 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const removeAccent = require("../util/removeAccent");
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: false,
+    default: "Một sản phẩm từ ..."
+  },
+  stock: {
+    type: Number,
+    required: true,
+    default: 10
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  size: {
+    type: [String],
+    required: true,
+    default: ["S", "L", "M", "X", "XL"]
+  },
+  productType: {
+    main: [String],
+    sub:  [String]
+  },
+  color: {
+    type: [String],
+    required: true,
+    default: ["Đỏ", "trắng"]
+  },
+  pattern: {
+    type: [String],
+    required: false
+  },
+  tags: {
+    type: [String],
+    required: false
+  },
+  images: {
+    type: [String],
+    required: true
+  },
+  dateAdded: {
+    type: Date,
+    required: false,
+    default: Date.now
+  },
+  isSale: {
+    status: {
+      type: Boolean,
+      default: false
+    },
+    percent: {
+      type: Number,
+      default: 0
+    },
+    end: {
+      type: Date
+    }
+  },
+  // ofSellers: {
+  //   userId: {
+  //     type: Schema.Types.ObjectId,
+  //     required: true,
+  //     ref: "User"
+  //   },
+  //   name: String
+  // },
+  labels: {
+    type: String,
+    required: false,
+    default: "Việt Nam"
+  },
+  materials: {
+    type: [String],
+    required: true,
+    defaut: ["vải", "lụa"]
+  },
+  buyCounts: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  viewCounts: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  rating: {
+    byUser: String,
+    content: String,
+    star: Number
+  },
+  index: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  comment: {
+    total: {
+      type: Number,
+      require: false,
+      default: 0
+    },
+    items: [
+      {
+        title: {
+          type: String
+        },
+        content: {
+          type: String
+        },
+        name: {
+          type: String
+        },
+        date: {
+          type: Date,
+          default: Date.now
+        },
+        star: {
+          type: Number
+        }
+      }
+    ]
+  }
+});
+
+const index = {
+  name: "text",
+  description: "text",
+  labels: "text",
+  "productType.main": "text",
+  tags: "text",
+  ofSellers: "text"
+};
+
+productSchema.index(index);
+
+productSchema.methods.getNonAccentType = function() {
+  return removeAccent(this.productType.main);
+};
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
